e30579e0091378fca4026d4e57c4804fb2318772
mugdha-gupta <mxg167030@utdallas.edu>
2020-05-01 08:55:59 -0500

    debug

--- src/ClientCommunicationInterface.java
+++ src/ClientCommunicationInterface.java
@@ -45,7 +45,7 @@ public class ClientCommunicationInterface implements Runnable {
 //            e.printStackTrace();
 //        }
 
-        if(client.clientId == 2){
+        if(client.clientId == 3){
             try {
                 sendToAll();
                 System.out.println("client " + client.clientId + "Sent to all");
