e480933a8996a1db676624697e7bcaba37da0169
mugdha-gupta <mxg167030@utdallas.edu>
2020-05-01 08:59:52 -0500

    changed message sending

--- src/ClientCommunicationInterface.java
+++ src/ClientCommunicationInterface.java
@@ -45,7 +45,7 @@ public class ClientCommunicationInterface implements Runnable {
 //            e.printStackTrace();
 //        }
 
-        if(client.clientId == 4){
+        if(client.clientId == 5){
             try {
                 sendToAll();
                 System.out.println("client " + client.clientId + "Sent to all");
